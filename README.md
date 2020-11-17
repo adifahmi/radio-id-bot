1e01f99869e5365ed24de3149e79af818fa20e07
Adi Fahmi <adifahmi379@gmail.com>
2020-11-17 10:19:58 +0700

    add ping and about command, fix some play issue
    also update README

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Radio-Id Bot (Discord Voice Bot)
 
-> RadioIdBot Discord Music Bot built with discord.py to play a radio from some Indonesian radio-station
+> Radio-id-bot is a simple Discord Music Bot built with discord.py to play a radio from some Indonesian radio-station
 
 ## Getting Started
 
 Invite this bot to your server with this [link 🔗](https://discord.com/api/oauth2/authorize?client_id=777757482687922198&permissions=8&scope=bot)
-
 
 ## Commands
 
@@ -13,6 +12,9 @@ Note: The default prefix is `!radio`
 
 * `!radioid help`
 List all available commands
+
+* `!radioid ping`
+Check bot discord latency
 
 * `!radioid join`
 Join a voice channel
@@ -29,8 +31,26 @@ Stop the radio
 * `!radioid leave`
 Leave voice channel
 
-
-## Ref
+## References
 * https://discordpy.readthedocs.io/en/latest/
 * https://stackoverflow.com/a/62495928/4844294
 * https://realpython.com/how-to-make-a-discord-bot-python/
+
+## Prerequisite to run/host this bot:
+* Python 3.5+
+* [FFMPEG](https://ffmpeg.org/download.html)
+
+Example on Ubuntu:
+
+    sudo apt install ffmpeg
+    virtualenv -p python3.6 venv
+    . venv/bin/activate
+    pip install -r requirements.txt
+
+don't forget to create .env file with your discord bot token then run the python
+
+    python main.py
+    
+## About
+
+* Contact me on [twitter](https://twitter.com/adifahmii)
--- main.py
+++ main.py
@@ -10,8 +10,11 @@ from static import get_radio_stream, get_radio_list
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+if TOKEN is None:
+    print("Please state your discord bot token in .env")
+    exit()
 
-bot = commands.Bot(command_prefix="!radio ", description="A bot to play your Indonesian radio station")
+bot = commands.Bot(command_prefix="!radio ", description="A bot to play Indonesian radio station")
 
 @bot.event
 async def on_ready():
@@ -19,6 +22,7 @@ async def on_ready():
     print(f"Currently added by {len(bot.guilds)} servers")
 
 
+@commands.cooldown(rate=1, per=3, type=commands.BucketType.guild)
 @bot.command(name="join")
 async def _join(ctx, *, channel: discord.VoiceChannel=None):
     """
@@ -30,6 +34,7 @@ async def _join(ctx, *, channel: discord.VoiceChannel=None):
         will be made.
     """
 
+    # if channel is not passed, will try to connect to same channel as author
     if not channel:
         try:
             channel = ctx.author.voice.channel
@@ -41,7 +46,8 @@ async def _join(ctx, *, channel: discord.VoiceChannel=None):
     try:
         # check if already connected to vc/no
         if vc:
-            # check if on same channel as author
+            # check if bot is not on same channel as author
+            # then move it
             if vc.channel.id != channel.id:
                 await vc.move_to(channel)
                 await ctx.send(f"Moved to: **{channel}**")
@@ -53,19 +59,23 @@ async def _join(ctx, *, channel: discord.VoiceChannel=None):
         return
 
 
+@commands.cooldown(rate=1, per=3, type=commands.BucketType.guild)
 @bot.command("list")
 async def _list(ctx):
     """
     List of available radio stations
     """
 
-    stations = ", ".join(get_radio_list())
-    await ctx.send(f"List of available radio {stations}")
+    message = "List of available stations:\n"
+    message += "\n".join([f":radio: {radio}" for radio in get_radio_list()])
+    message += "\nuse `!radio play {station}` to start playing it"
+    await ctx.send(message)
     return
 
 
+@commands.cooldown(rate=1, per=10, type=commands.BucketType.guild)
 @bot.command(name="play")
-async def _play(ctx, station, channel: discord.VoiceChannel=None):
+async def _play(ctx, station):
     """
     Play a radio station based on user input
     """
@@ -74,7 +84,7 @@ async def _play(ctx, station, channel: discord.VoiceChannel=None):
         source = get_radio_stream(station)
 
         if source is None:
-            await ctx.send(f"Unknown station {station}, use !radio list to get available station")
+            await ctx.send(f"Unknown station {station}, use `!radio list` to get list of available station")
             return
 
         try:
@@ -83,13 +93,16 @@ async def _play(ctx, station, channel: discord.VoiceChannel=None):
             await ctx.send("You need to be in voice channel")
             return
 
-        print(f"Initiate radio play on {channel}, station: {station}")
+        print(f"Initiate radio play on {ctx.guild.name} - {channel}, station: {station}")
 
         vc = ctx.voice_client
+
         try:
             # check if already connected to vc/no
             if vc:
-                # check if on same channel as author
+                if vc.is_playing() is True:
+                    await ctx.send("Radio is already playing a station, use `!radio stop` to stop current station and reinitiate `!radio play`")
+                    return
                 if vc.channel.id != channel.id:
                     await vc.move_to(channel)
                     await ctx.send(f"Moved to: **{channel}**")
@@ -98,16 +111,37 @@ async def _play(ctx, station, channel: discord.VoiceChannel=None):
                 vc = ctx.voice_client
                 await ctx.send(f"Connected to: **{channel}**")
         except asyncio.TimeoutError:
-            await ctx.send(f"Connecting to channel: <{channel}> timed out.")
+            await ctx.send(f"Connecting to channel: <{channel}> timed out")
             return
 
-        await ctx.send(f"Start playing {station} ...")
+        await ctx.send(f"Start playing {station} :loud_sound:")
         time.sleep(1)
-        vc.play(discord.FFmpegPCMAudio(source), after=lambda e: print("Media play stopped/done"))
+        vc.play(discord.FFmpegPCMAudio(source), after=lambda e: print("Media play stop"))
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=station))
     except Exception as e:
         await ctx.send(f"A client exception occured:\n`{e}`")
 
 
+@bot.command("playing")
+async def _playing(ctx):
+    """
+    Get what's playing now, if any
+    """
+    vc = ctx.voice_client
+
+    if not vc:
+        await ctx.send("Radio not in a voice channel")
+        return
+
+    if vc.is_playing() is False:
+        await ctx.send("Radio not playing anything")
+        return
+    
+    print(vc.source)
+    await ctx.send(f"Radio is playing {vc.source}")
+
+
+@commands.cooldown(rate=1, per=3, type=commands.BucketType.guild)
 @bot.command("stop")
 async def _stop(ctx):
     """
@@ -116,15 +150,21 @@ async def _stop(ctx):
     vc = ctx.voice_client
 
     if not vc:
-        await ctx.send("Already stopped.")
+        await ctx.send("Radio not in a voice channel")
+        return
+    
+    if vc.is_playing() is False:
+        await ctx.send("Radio not playing anything")
         return
 
     await ctx.send("Stopping...")
     vc.stop()
     time.sleep(3)
-    await ctx.send("Radio stopped!")
+    await bot.change_presence(activity=None, status=discord.Status.online)
+    await ctx.send("Radio stopped")
 
 
+@commands.cooldown(rate=1, per=3, type=commands.BucketType.guild)
 @bot.command("leave")
 async def _disconnect(ctx):
     """
@@ -133,12 +173,38 @@ async def _disconnect(ctx):
     vc = ctx.voice_client
 
     if not vc:
-        await ctx.send("Radio not in a voice channel.")
+        await ctx.send("Radio not in a voice channel")
         return
 
     await vc.disconnect()
     time.sleep(2)
-    await ctx.send("Radio have left the voice channel!")
+    await bot.change_presence(activity=None, status=discord.Status.online)
+    await ctx.send("Radio have left the voice channel")
+
+
+@bot.command("ping")
+async def _ping(ctx):
+    """
+    Check latency between a HEARTBEAT and a HEARTBEAT_ACK in seconds
+    """
+    vc = ctx.voice_client
+
+    if not vc:
+        await ctx.send("Radio not in a voice channel")
+        return
+    
+    latency = vc.latency
+    await ctx.send(f"Radio bot voice latency is {latency} seconds")
+
+
+@bot.command("about")
+async def _about(ctx):
+    """
+    Print contact info
+    """
+    embed = discord.Embed()
+    embed.description = "Created by [AF](https://twitter.com/adifahmii), source code is available on [Github](https://github.com/AdiFahmi/radio-id-bot)"
+    await ctx.send(embed=embed)
 
 
 @_play.error
@@ -149,6 +215,13 @@ async def info_error(ctx, error):
         except AttributeError:
             await ctx.send("You need to be in voice channel")
             return
-        await ctx.send(f"Please specify radio station, use !radio list to get available station")
+        await ctx.send(f"Please specify radio station, use `!radio list` to get list of available station")
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        await ctx.send("This command is on a %.2fs cooldown" % error.retry_after)
+        return
+    raise error
 
 bot.run(TOKEN)
