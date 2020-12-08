8d7feb3456241b9ff4bd531544d10d6e91df4170
Adi Fahmi <adifahmi379@gmail.com>
2020-12-08 17:53:20 +0700

    paginate guild list with reaction

--- app/misc.py
+++ app/misc.py
@@ -1,10 +1,11 @@
+import asyncio
 import discord
 
 from discord.ext import commands
 from tabulate import tabulate
 
 from .player import NOW_PLAYING
-from .utils import chunk_list
+from .utils import chunk_list, get_page
 
 
 class Misc(commands.Cog):
@@ -29,6 +30,18 @@ class Misc(commands.Cog):
         await ctx.send(f"Status changed to:\n{status}")
         return
 
+    async def page_reaction(self, msg, total_page, current_page):
+        if total_page == 1:
+            return
+        if current_page == 1:
+            await msg.add_reaction('⏩')
+        elif current_page == total_page:
+            await msg.add_reaction('⏪')
+        else:
+            await msg.add_reaction('⏪')
+            await msg.add_reaction('⏩')
+        return
+
     @commands.is_owner()
     @commands.command("stats", hidden=True)
     async def _stats(self, ctx):
@@ -43,15 +56,43 @@ class Misc(commands.Cog):
         await ctx.send("List of servers:")
         total_member = 0
         num = 1
+        page = 1
+        guild_table = {}
         for guilds in chunk_guild:
             guild_list = []
             for guild in guilds:
                 guild_list.append([num, guild.name, guild.member_count])
                 total_member += guild.member_count
                 num += 1
-            await ctx.send(f'```{tabulate(guild_list, tablefmt="fancy_grid")}```')
+            formatted_guilds = f'```{tabulate(guild_list, tablefmt="fancy_grid")}```'
+            guild_table[page] = formatted_guilds
+            page += 1
+
+        total_page = len(guild_table)
+        current_page = 1
+
+        msg = await ctx.send(f"{guild_table[current_page]} Page {current_page} of {total_page} ")
+        await self.page_reaction(msg, total_page, current_page)
 
         await ctx.send(f"Total members: {total_member}")
+        await asyncio.sleep(1)
+
+        while True:
+            try:
+                reaction, _ = await self.bot.wait_for('reaction_add', timeout=5.0)
+                await msg.clear_reactions()
+                current_page = get_page(current_page, reaction)
+                await msg.edit(content=f"{guild_table[current_page]} Page {current_page} of {total_page} ")
+                await self.page_reaction(msg, total_page, current_page)
+
+                await asyncio.sleep(1)
+            except asyncio.TimeoutError:
+                await msg.clear_reactions()
+                break
+            except Exception:
+                await msg.clear_reactions()
+                break
+
         return
 
     @commands.is_owner()
--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,28 @@
 import random
 import string
 
+EMOJI_NUMBER = {
+    0: '0️⃣',
+    1: '1️⃣',
+    2: '2️⃣',
+    3: '3️⃣',
+    4: '4️⃣',
+    5: '5️⃣',
+    6: '6️⃣',
+    7: '7️⃣',
+    8: '8️⃣',
+    9: '9️⃣',
+}
+
 
 def generate_random_string(n=10):
-    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=n))
+    r = ''.join(
+        random.choices(
+            string.ascii_uppercase + string.digits,
+            k=n
+        )
+    )
+    return r
 
 
 def chunk_list(seq, num):
@@ -16,3 +35,39 @@ def chunk_list(seq, num):
         last += avg
 
     return out
+
+
+class DummyGuilds:
+    def __init__(self, name, member_count):
+        self.name = name
+        self.member_count = member_count
+
+
+def dummy_guilds(n=10):
+    guilds = []
+    for _ in range(n):
+        guilds.append(
+            DummyGuilds(
+                generate_random_string(random.randint(5, 10)),
+                random.randint(10, 1000)
+            )
+        )
+    return guilds
+
+
+def get_emoji_by_number(num):
+    return EMOJI_NUMBER.get(num)
+
+
+def get_number_by_emoji(emoji):
+    for k, v in EMOJI_NUMBER.items():
+        if str(emoji) == v:
+            return k
+    return None
+
+
+def get_page(current_page, emoji):
+    if str(emoji) == '⏩':
+        return current_page + 1
+    else:
+        return current_page - 1
