ac5a1894d69e183247016d0b4d74d1962482d1f8
Adi Fahmi <adifahmi379@gmail.com>
2021-03-12 17:01:31 +0700

    forcing get new content

--- update-station.sh
+++ update-station.sh
@@ -4,5 +4,5 @@ URL="HTTP_URL_TO_FILE"
 STATIONS_FILE="stations.yaml"
 
 rm $STATIONS_FILE
-wget $URL -O $STATIONS_FILE
+wget --no-check-certificate --no-cache --no-cookies $URL -O $STATIONS_FILE
 cat $STATIONS_FILE
