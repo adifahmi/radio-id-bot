849b8518f0d44cc0a89495a2adffcbc7588cdc56
Adi Fahmi <adifahmi379@gmail.com>
2021-03-12 17:11:06 +0700

    generate random string to always ignore cache

--- update-station.sh
+++ update-station.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-URL="HTTP_URL_TO_FILE"
+URL="HTTP_URL"
 STATIONS_FILE="stations.yaml"
+RANDOM_STRING=`openssl rand -hex 5`
+URL_NO_CACHE="${URL}?cachebust=${RANDOM_STRING}"
 
 rm $STATIONS_FILE
-wget --no-check-certificate --no-cache --no-cookies $URL -O $STATIONS_FILE
+wget --no-check-certificate --no-cache --no-cookies $URL_NO_CACHE -O $STATIONS_FILE
 cat $STATIONS_FILE
