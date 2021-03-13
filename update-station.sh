dce7c9218cb063d48861c73245868d2a343b01c7
Adi Fahmi <adifahmi379@gmail.com>
2021-03-13 15:23:09 +0700

    change how to url and smal update on script flow

--- update-station.sh
+++ update-station.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
-URL="HTTP_URL"
+# read .env file
+export $(egrep -v '^#' .env | xargs)
+
+URL=$YOUR_GIST_URL
 STATIONS_FILE="stations.yaml"
 RANDOM_STRING=`openssl rand -hex 5`
 URL_NO_CACHE="${URL}?cachebust=${RANDOM_STRING}"
 
-rm $STATIONS_FILE
-wget --no-check-certificate --no-cache --no-cookies $URL_NO_CACHE -O $STATIONS_FILE
-cat $STATIONS_FILE
+mv $STATIONS_FILE "{$STATIONS_FILE}_temp"
+if wget --no-check-certificate --no-cache --no-cookies $URL_NO_CACHE -O $STATIONS_FILE; then
+    echo "Success getting new file"
+    rm "{$STATIONS_FILE}_temp"
+    echo "old file deleted"
+else
+    echo "WGET failed, revert action"
+    mv "{$STATIONS_FILE}_temp" $STATIONS_FILE
+fi
+
+echo "Done"
