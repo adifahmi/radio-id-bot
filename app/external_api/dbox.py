6e3e70b1abe8db2853ca60e319ae860a03780376
Adi Fahmi <adifahmi379@gmail.com>
2021-03-17 21:56:20 +0700

    add dropbox api helpers

---
+++ app/external_api/dbox.py
@@ -0,0 +1,48 @@
+import os
+
+from dotenv import load_dotenv
+from .base import api
+
+load_dotenv()
+
+HOST = lambda: 'https://api.dropboxapi.com/2/'
+CONTENT_HOST = lambda: 'https://content.dropboxapi.com/2/'
+
+BASE_HEADERS = {
+    'Authorization': f'Bearer {os.getenv("DBOX_TOKEN")}',
+    'Cache-Control': 'no-cache'
+}
+
+
+def upload_file(file, filename):
+    headers = dict(BASE_HEADERS, **{
+        'Content-Type': 'application/octet-stream',
+        'Dropbox-API-Arg': f'{{"path": "/APIUpload/RadioID/{filename}","mode": "add","autorename": true,"mute": false,"strict_conflict": false}}'
+    })
+    payload = file
+
+    dbox_api = api(CONTENT_HOST, headers)
+    result = dbox_api('post', 'files/upload', payload, False)
+    return result
+
+
+def create_share_link(file_path):
+    headers = dict(BASE_HEADERS, **{
+        'Content-Type': 'application/json',
+    })
+    payload = f'{{"path": "{file_path}"}}'
+
+    dbox_api = api(HOST, headers)
+    result = dbox_api('post', 'sharing/create_shared_link_with_settings', payload, False)
+    return result
+
+
+def get_link(file_id):
+    headers = dict(BASE_HEADERS, **{
+        'Content-Type': 'application/json',
+    })
+    payload = f'{{"path": "{file_id}"}}'
+
+    dbox_api = api(HOST, headers)
+    result = dbox_api('post', 'sharing/list_shared_links', payload, False)
+    return result
