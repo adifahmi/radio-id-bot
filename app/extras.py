c462e8d45b9ec26b07c3d76cf3b6a3c771f2b951
Adi Fahmi <adifahmi379@gmail.com>
2021-03-17 21:57:03 +0700

    add upload_stats cmd to upload stats to dropbox

--- app/extras.py
+++ app/extras.py
@@ -1,7 +1,10 @@
 import discord
+import os
+import datetime
 
 from discord.ext import commands
-from .external_api import ksoft
+from .utils import create_tempfile, dummy_guilds
+from .external_api import ksoft, dbox
 
 
 class Extras(commands.Cog):
@@ -44,3 +47,49 @@ class Extras(commands.Cog):
         embed = discord.Embed(title=song, description=lyrics)
         embed.set_footer(text="Lyrics provided by KSoft.Si")
         await ctx.send(embed=embed)
+
+    @commands.is_owner()
+    @commands.command("upload_stats", hidden=True)
+    async def _upload_stats(self, ctx):
+        """
+        Show some stats of this bot (owner only)
+        """
+
+        guild_obj = self.bot.guilds
+        guild_obj = dummy_guilds(1027)
+        total_guild = len(guild_obj)
+
+        # prepare csv
+        fmt_full_report = f"Added by {total_guild} servers\n\n"
+        fmt_full_report += "id,name,member_cnt,guild_id\n"
+
+        await ctx.send("Preparing data ...")
+        total_member = 0
+        num = 1
+        for guild in guild_obj:
+            fmt_full_report += f"{num},{guild.name},{guild.member_count},{guild.id}\n"
+            total_member += guild.member_count
+            num += 1
+
+        fmt_full_report += f"\nTotal members: {total_member}"
+        now = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M")
+
+        file = create_tempfile(fmt_full_report)
+        env = os.environ.get("ENVIRONMENT")
+        filename = f"RadioID_{env}_{now}.csv"
+
+        await ctx.send("Uploading stats to dropbox")
+        ul, ul_info = dbox.upload_file(file, filename)
+        if ul_info['status_code'] != 200:
+            await ctx.send(f"Failed to upload ```{str(ul_info['error'])}```")
+            return
+
+        await ctx.send(f"File uploaded at `{ul.get('path_display')}`, getting link ...")
+
+        gl, gl_info = dbox.create_share_link(ul.get('path_display'))
+        if gl_info['status_code'] != 200:
+            await ctx.send(f"Failed to get download link ```{str(gl_info['error'])}```")
+        else:
+            await ctx.send(f"Download link: {gl.get('url')}")
+
+        return
