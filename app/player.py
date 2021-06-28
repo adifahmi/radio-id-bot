ff36cc69e4ea8da0fe330c2ebcba3b741e41ae9d
Adi Fahmi <adifahmi379@gmail.com>
2021-06-28 19:14:55 +0700

    reenable promo msg

--- app/player.py
+++ app/player.py
@@ -155,8 +155,7 @@ class RadioPlayer(commands.Cog):
                 if vc.is_playing():
                     # send promo message once at a time in a session
                     if already_promote is False:
-                        pass
-                        # await ctx.send(f"Tahukan kamu? sekarang kamu bisa bantu donasi untuk pengembangan bot ini melalui link saweria di `{self.prefix} donate` :innocent:")
+                        await ctx.send(f"Tahukan kamu? sekarang kamu bisa bantu donasi untuk biaya hosting bot ini melalui link saweria di `{self.prefix} donate` :innocent:")
                     already_promote = True
 
                     await asyncio.sleep(5)
